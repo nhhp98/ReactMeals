fa57a3f666c94918c31a930773da265207cfd94c
ADMIN <nguyenhuynhhongphuc.work@gmail.com>
2021-09-17 19:17:03 +0700

    adding MealItem.js

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import classes from './MealItem.module.css'
+
+const MealItem = (props) => {
+
+    const price = `$${props.price.toFixed(2)}`
+    return (
+
+        <li className={classes.meal}>
+            <div >
+                <h3>{props.name}</h3>
+                <h3 className={classes.description}>{props.description}</h3>
+                <h3 className={classes.price}>{price}</h3>
+            </div>
+            <div>
+
+            </div>
+        </li>
+    )
+}
+
+export default MealItem
