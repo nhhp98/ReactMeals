9e7ef8cb63cb5c2b3847acb16180894ac164999e
ADMIN <nguyenhuynhhongphuc.work@gmail.com>
2021-09-18 23:12:20 +0700

    adding & remove meal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,13 @@ const Cart = (props) => {
 
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = () => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItems(id)
+  };
 
-  const cartItemAddHandler = () => {};
+  const cartItemAddHandler = (item) => {
+      cartCtx.addItem({...item, amount: 1})
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -23,8 +27,8 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler(null, item.id)}
-          onAdd={cartItemAddHandler(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
