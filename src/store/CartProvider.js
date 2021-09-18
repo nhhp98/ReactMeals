b30a92ab890b22524bd62c4fb31d3388b10509da
ADMIN <nguyenhuynhhongphuc.work@gmail.com>
2021-09-18 22:05:39 +0700

    merge duplicate meals

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,10 +9,31 @@ const defaultCarState = {
 const cartReducer = (state, action) => {
 
     if(action.type === 'ADD'){
-        const updateItems = state.items.concat(action.item)
+        
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+
+        const existingCartItem = state.items[existingCartItemIndex]
+
+       
+        let updatedItems
+
+        if(existingCartItem){
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }else{
+
+            updatedItems = state.items.concat(action.item)
+        }
+
+        
         return {
-            items: updateItems,
+            items: updatedItems,
             totalAmount: updatedTotalAmount,
         }
     }
